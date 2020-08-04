20cefed44e6c67468a41e0199233ce8fe26fd0e2
Nicolas <nico.iriondo@hotmail.com>
2020-08-04 09:08:38 -0300

    Mod en INDEX

--- js/main.js
+++ js/main.js
@@ -27,113 +27,117 @@
         var camisas = document.getElementById('camisa_evento');
         var etiquetas = document.getElementById('etiquetas');
 
-        calcular.addEventListener('click', calcularMontos);
+        if (document.getElementById('calcular')) {
+            calcular.addEventListener('click', calcularMontos);
 
-        pase_dia.addEventListener('blur', mostrarDias);
-        pase_dosdias.addEventListener('blur', mostrarDias);
-        pase_completo.addEventListener('blur', mostrarDias);
+            calcular.addEventListener('click', calcularMontos);
 
-        nombre.addEventListener('blur', validarCampos);
-        apellido.addEventListener('blur', validarCampos);
-        email.addEventListener('blur', validarCampos);
-        email.addEventListener('blur', validarMail);
+            pase_dia.addEventListener('blur', mostrarDias);
+            pase_dosdias.addEventListener('blur', mostrarDias);
+            pase_completo.addEventListener('blur', mostrarDias);
+
+            nombre.addEventListener('blur', validarCampos);
+            apellido.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarMail);
 
 
 
-        function validarCampos() {
+            function validarCampos() {
 
-            if (this.value == "") {
-                errorDiv.style.display = 'block';
-                errorDiv.innerHTML = "Este campo es obligatorio";
-                this.style.border = '1px solid red';
-                errorDiv.style.border = '1px solid red';
-            } else {
-                errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
+                if (this.value == "") {
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = "Este campo es obligatorio";
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                } else {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
+                }
+
             }
 
-        }
+            function validarMail() {
+                if (this.value.indexOf("@") > -1) {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
 
-        function validarMail() {
-            if (this.value.indexOf("@") > -1) {
-                errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
+                } else {
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = "Por favor ingrese un email valido";
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                }
 
-            } else {
-                errorDiv.style.display = 'block';
-                errorDiv.innerHTML = "Por favor ingrese un email valido";
-                this.style.border = '1px solid red';
-                errorDiv.style.border = '1px solid red';
             }
 
-        }
+
+            function calcularMontos(event) {
+
+                event.preventDefault();
+                if (regalo.value == '') {
+                    alert('Debes elegir un regalo');
+                    regalo.focus();
+                } else {
+                    var boletosDia = parseInt(pase_dia.value, 10) || 0,
+                        boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
+                        boletoCompleto = parseInt(pase_completo.value, 10) || 0,
+                        cantCamisas = parseInt(camisas.value, 10) || 0,
+                        cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
 
 
-        function calcularMontos(event) {
+                    var totalPagar = (boletosDia * 300) + (boletos2Dias * 450) + (boletoCompleto * 500) + ((cantCamisas * 150) * 0.90) + (cantEtiquetas * 25);
 
-            event.preventDefault();
-            if (regalo.value == '') {
-                alert('Debes elegir un regalo');
-                regalo.focus();
-            } else {
+                    var listadoProductos = [];
+
+                    if (boletosDia >= 1) {
+                        listadoProductos.push(boletosDia + ' Pases por dia');
+                    }
+                    if (boletos2Dias >= 1) {
+                        listadoProductos.push(boletos2Dias + ' Pases por 2 dias');
+                    }
+                    if (boletoCompleto >= 1) {
+                        listadoProductos.push(boletoCompleto + ' Pases Completos');
+                    }
+                    if (cantCamisas >= 1) {
+                        listadoProductos.push(cantCamisas + ' Camisas');
+                    }
+                    if (cantEtiquetas >= 1) {
+                        listadoProductos.push(cantEtiquetas + ' Etiquetas');
+
+                    }
+                    lista_productos.style.display = 'block';
+                    lista_productos.innerHTML = '';
+
+                    for (var i = 0; i < listadoProductos.length; i++) {
+                        lista_productos.innerHTML += listadoProductos[i] + '<br/>';
+                    }
+
+                    suma.innerHTML = "$ " + totalPagar.toFixed(2);
+                }
+
+            }
+
+            function mostrarDias() {
                 var boletosDia = parseInt(pase_dia.value, 10) || 0,
                     boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
-                    boletoCompleto = parseInt(pase_completo.value, 10) || 0,
-                    cantCamisas = parseInt(camisas.value, 10) || 0,
-                    cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+                    boletoCompleto = parseInt(pase_completo.value, 10) || 0;
 
+                var diasElegidos = [];
 
-                var totalPagar = (boletosDia * 300) + (boletos2Dias * 450) + (boletoCompleto * 500) + ((cantCamisas * 150) * 0.90) + (cantEtiquetas * 25);
-
-                var listadoProductos = [];
-
-                if (boletosDia >= 1) {
-                    listadoProductos.push(boletosDia + ' Pases por dia');
+                if (boletosDia > 0) {
+                    diasElegidos.push('viernes');
                 }
-                if (boletos2Dias >= 1) {
-                    listadoProductos.push(boletos2Dias + ' Pases por 2 dias');
+                if (boletos2Dias > 0) {
+                    diasElegidos.push('viernes', 'sabado');
                 }
-                if (boletoCompleto >= 1) {
-                    listadoProductos.push(boletoCompleto + ' Pases Completos');
-                }
-                if (cantCamisas >= 1) {
-                    listadoProductos.push(cantCamisas + ' Camisas');
-                }
-                if (cantEtiquetas >= 1) {
-                    listadoProductos.push(cantEtiquetas + ' Etiquetas');
-
-                }
-                lista_productos.style.display = 'block';
-                lista_productos.innerHTML = '';
-
-                for (var i = 0; i < listadoProductos.length; i++) {
-                    lista_productos.innerHTML += listadoProductos[i] + '<br/>';
+                if (boletoCompleto > 0) {
+                    diasElegidos.push('viernes', 'sabado', 'domingo');
                 }
 
-                suma.innerHTML = "$ " + totalPagar.toFixed(2);
-            }
-
-        }
-
-        function mostrarDias() {
-            var boletosDia = parseInt(pase_dia.value, 10) || 0,
-                boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
-                boletoCompleto = parseInt(pase_completo.value, 10) || 0;
-
-            var diasElegidos = [];
-
-            if (boletosDia > 0) {
-                diasElegidos.push('viernes');
-            }
-            if (boletos2Dias > 0) {
-                diasElegidos.push('viernes', 'sabado');
-            }
-            if (boletoCompleto > 0) {
-                diasElegidos.push('viernes', 'sabado', 'domingo');
-            }
-
-            for (var i = 0; i < diasElegidos.length; i++) {
-                document.getElementById(diasElegidos[i]).style.display = "block";
+                for (var i = 0; i < diasElegidos.length; i++) {
+                    document.getElementById(diasElegidos[i]).style.display = "block";
+                }
             }
         }
 
@@ -142,6 +146,37 @@
 
 
 $(function() {
+
+
+    // Animacion Titulo
+
+    $('.nombre-sitio').lettering();
+
+    // Menu - fijo 
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({ 'margin-top': barraAltura + 'px' })
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({
+                'margin-top': '0px'
+            });
+        }
+    })
+
+    // Menu responsive
+
+    $('.menu-movil').on('click', function() {
+        $('.navegacion-principal').slideToggle();
+    });
+
+
+
 
     //PRogama de Conferencias
     $('.programa-evento .info-curso:first').show();
@@ -157,6 +192,28 @@ $(function() {
         $(enlace).fadeIn(1000);
         return false;
 
-    })
+    });
+
+    // Animaciones para los numeros
+
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6 }, 2000);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15 }, 2000);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 2000);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9 }, 2000);
+
+
+    // Animacion Cuenta Regresiva
+
+    $('.cuenta-regresiva').countdown('2020/12/10 09:00:00',
+        function(event) {
+            $('#dias').html(event.strftime('%D'));
+            $('#horas').html(event.strftime('%H'));
+            $('#minutos').html(event.strftime('%M'));
+            $('#segundos').html(event.strftime('%S'));
+
+        });
+
+
+
 
 });
