9039a67817f16ea91bb3a3c54200789d3b9b8cd3
Nicolas <nico.iriondo@hotmail.com>
2020-08-07 11:30:17 -0300

    Creacion de Templates

---
+++ includes/templates/footer.php
@@ -0,0 +1,64 @@
+
+    <footer class="site-footer">
+        <div class="contenedor clearfix">
+            <div class="footer-informacion">
+                <h3>Sobre <span>PdeWebCamp</span></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, asperiores voluptatem, cum incidunt ipsum natus distinctio quisquam reiciendis autem qui aliquid iusto consectetur, quos quo quia ex? Tenetur, amet deleniti.</p>
+            </div>
+            <div class="ultimos-tweets">
+                <h3>Ultimos <span>Tweets</span></h3>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                </ul>
+            </div>
+            <div class="menu">
+                <h3>REdes <span>Sociales</span></h3>
+                <nav class="redes-sociales">
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-pinterest-p"></i></a>
+                    <a href=""><i class="fab fa-youtube"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                </nav>
+            </div>
+
+        </div>
+
+
+        <p class="copyright">Todos los derechos Reservados PDEWEBCAMP 2020</p>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js
+  "></script>
+    <script>
+        window.jQuery || document.write('<script src="js/vendor/jquery-3.5.1.min.js"><\/script>')
+    </script>
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/mapa.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.lettering.js"></script>
+    <script src="js/lightbox.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function() {
+            ga.q.push(arguments)
+        };
+        ga.q = [];
+        ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('set', 'transport', 'beacon');
+        ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
